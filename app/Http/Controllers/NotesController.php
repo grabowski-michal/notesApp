8f6bd0dd6e4134062d186e806717f81d2e73d4ff
grabowski-michal <michal.grabowski98@gmail.com>
2021-02-13 21:14:09 +0100

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   .styleci.yml 	new file:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/NotesController.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Http/Requests/NoteRequest.php 	new file:   app/Models/Note.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_09_27_212641_create_versions_table.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2021_02_13_155038_create_notes_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/css/custom_style.css 	new file:   public/favicon.ico 	new file:   public/img/edit.png 	new file:   public/img/history.png 	new file:   public/img/remove.png 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   public/web.config 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/lang/en/auth.php 	new file:   resources/lang/en/pagination.php 	new file:   resources/lang/en/passwords.php 	new file:   resources/lang/en/validation.php 	new file:   resources/views/noteAddForm.blade.php 	new file:   resources/views/notes.blade.php 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   server.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   webpack.mix.js

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\NoteRequest;
+use App\Models\Note;
+
+class NotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::orderBy('created_at', 'asc')->get();
+        return view('notes', compact('notes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $note = new Note();
+        return view('noteAddForm', compact('note'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(NoteRequest $request)
+    {
+        $note = new Note();
+        $note->title = $request->title;
+        $note->content = $request->content;
+        if ($note->save()) {
+            return redirect()->route('notes');
+        }
+        return "An error has occured while saving the note (enable MySQL DB!).";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Show the history of specified note.
+     * 
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function history($id) {
+
+    }
+}
